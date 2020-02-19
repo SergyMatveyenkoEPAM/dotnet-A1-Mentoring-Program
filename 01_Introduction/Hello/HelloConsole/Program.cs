db7097a28b2bb58aa582d5d2aae087e9190c1c6a
Sergey Matveyenko <v-smatveyenka@ebsco.com>
2020-02-19 18:44:08 +0300

    console app has been created

---
+++ 01_Introduction/Hello/HelloConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace HelloConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Any())
+            {
+                string name = args[0];
+                System.Console.WriteLine($"Hello, {name}!");
+            }
+        }
+    }
+}
