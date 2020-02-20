1436245acbaeeb5d9db62912affd57651f820efd
Sergey Matveyenko <v-smatveyenka@ebsco.com>
2020-02-20 10:35:35 +0300

    HelloWinForms

---
+++ 01_Introduction/Hello/HelloWinForms/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Hello, {textBox1.Text}!", "Hello", MessageBoxButtons.OK);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+                button1.Enabled = false;
+            else
+            {
+                button1.Enabled = true;
+            }
+        }
+    }
+}
