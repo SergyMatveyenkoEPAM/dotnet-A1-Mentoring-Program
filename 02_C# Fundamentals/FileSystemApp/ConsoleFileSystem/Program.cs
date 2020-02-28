1c4b61cec6f0f92187ce83003ec341bb27a0ec83
Sergey Matveyenko <v-smatveyenka@ebsco.com>
2020-02-28 19:36:59 +0300

    Methods module

---
+++ 02_C# Fundamentals/FileSystemApp/ConsoleFileSystem/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Channels;
+using FileSystemApp;
+
+namespace ConsoleFileSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string startDirectory = @"d:\A1 Mentoring Program\02_C# Fundamentals\FileSystemApp\ConsoleFileSystem\";
+
+            FileSystemVisitor fileSystemVisitor = new FileSystemVisitor(address => !address.Contains("Debug"));
+
+            fileSystemVisitor.StartMessage += message => Console.WriteLine(message);
+            fileSystemVisitor.EndMessage += message => Console.WriteLine(message);
+            fileSystemVisitor.DirectoryFound += message => Console.WriteLine(message);
+            fileSystemVisitor.FilteredDirectoryFound += message => Console.WriteLine(message);
+            fileSystemVisitor.FileFound += message => Console.WriteLine(message);
+            fileSystemVisitor.FilteredFileFound += message => Console.WriteLine(message);
+
+            if (string.IsNullOrEmpty(startDirectory) || !Directory.Exists(startDirectory))
+            {
+                Console.WriteLine("Path to the start directory is incorrect");
+            }
+
+            // without ".ToList()" we can not get the correct order of notification
+            var results = fileSystemVisitor.GetAllFoldersAndFiles(startDirectory).ToList();
+            
+            Console.WriteLine("Searching results:");
+
+            foreach (var item in results)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 02_C# Fundamentals/FileSystemApp/FileSystemApp/FileSystemVisitor.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace FileSystemApp
+{
+    public delegate void StartMessage(string message);
+    public delegate void EndMessage(string message);
+    public delegate void FileFound(string message);
+    public delegate void FilteredFileFound(string message);
+    public delegate void DirectoryFound(string message);
+    public delegate void FilteredDirectoryFound(string message);
+
+    public class FileSystemVisitor
+    {
+        public event StartMessage StartMessage;
+        public event EndMessage EndMessage;
+        public event FileFound FileFound;
+        public event FilteredFileFound FilteredFileFound;
+        public event DirectoryFound DirectoryFound;
+        public event FilteredDirectoryFound FilteredDirectoryFound;
+        
+        public Predicate<string> Filter { get; set; }
+
+        public FileSystemVisitor()
+        {
+
+        }
+
+        public FileSystemVisitor(Predicate<string> filter)
+        {
+            Filter = filter;
+        }
+
+        public IEnumerable<string> GetAllFoldersAndFiles(string startAddress)
+        {
+            if (string.IsNullOrEmpty(startAddress) || !Directory.Exists(startAddress))
+            {
+                var flag = false;
+            }
+
+            StartMessage?.Invoke("-------------Searching was started-------------");
+
+            // without ".ToList()" we can not get the correct order of notification
+            var result = FindEntities(startAddress).ToList();
+
+            EndMessage?.Invoke("-------------Searching was finished-------------");
+
+            return result;
+        }
+
+        private IEnumerable<string> FindEntities(string startAddress)
+        {
+            var directories = Directory.GetDirectories(startAddress).Where(d =>
+            {
+                DirectoryFound?.Invoke("Directory was found: " + Path.GetFileName(d));
+
+                if (Filter != null)
+                {
+                    if (Filter(d))
+                    {
+                        FilteredDirectoryFound?.Invoke("Filtered directory was found: " + Path.GetFileName(d));
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+                // without ".ToList()" messages appears twice
+            }).ToList();
+
+            var files = Directory.GetFiles(startAddress).Where(f =>
+            {
+                FileFound?.Invoke("File was found: " + Path.GetFileName(f));
+
+                if (Filter != null)
+                {
+                    if (Filter(f))
+                    {
+                        FilteredFileFound?.Invoke("Filtered file was found: " + Path.GetFileName(f));
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+                // without ".ToList()" messages appears twice
+            }).ToList();
+
+            var entityes = directories.Concat(files).AsEnumerable();
+
+            foreach (var directory in directories)
+            {
+                entityes = entityes.Concat(FindEntities(directory));
+            }
+
+            return entityes;
+        }
+
+        //private IEnumerable<string> GetDirectories(string startAddress)
+        //{
+
+        //}
+    }
+}
