b351d958d775795937f1fbcefecfb2e81426186d
Sergey Matveyenko <qdm4@yandex.ru>
2020-03-01 20:45:12 +0300

    FileSystemAppLibrary.Tests has been added

---
+++ 02_C# Fundamentals/FileSystemApp/FileSystemAppLibrary.Tests/UnitTests/FileSystemVisitorTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileSystemAppLibrary.Tests.UnitTests
+{
+    [TestFixture]
+    class FileSystemVisitorTests
+    {
+        [Test]
+        public void Find_Request_True()
+        {
+            // Arrange
+            int a = 1;
+            int b = 1;
+            int c;
+
+            // Act
+            c = a + b;
+
+            //Assert
+            Assert.IsTrue(c == 2);
+        }
+
+    }
+}
