a00fb168422e28e9e1678f73cce3afa3ccf1190f
Sergey Matveyenko <v-smatveyenka@ebsco.com>
2020-03-10 12:41:32 +0300

    ExceptionHandlingLibrary has been added

---
+++ 02_C# Fundamentals/ExceptionHandlingApp/ExceptionHandlingLibrary/ConverterToInt32.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExceptionHandlingLibrary
+{
+    public class ConverterToInt32
+    {
+        public static int GetInt32(string number)
+        {
+            try
+            {
+                return Convert.ToInt32(number);
+            }
+            catch (FormatException e)
+            {
+                throw new ConvertException("Input string is not a sequence of digits.");
+            }
+            catch (OverflowException e)
+            {
+                throw new ConvertException("The number cannot fit in an Int32.");
+            }
+        }
+    }
+}
