059ba709be0e69526fd4baf5dbb5f0f64a8d58e7
Sergey Matveyenko <v-smatveyenka@ebsco.com>
2020-03-18 13:36:52 +0300

    .gitignore has been updated

---
+++ 03_Basic SQL/Scripts/1.1.1_Orders_after_6.5.98.sql
@@ -0,0 +1,3 @@
+﻿SELECT OrderID, ShippedDate, ShipVia 
+  FROM [dbo].[Orders]
+  WHERE ShippedDate>=convert(datetime,'05.06.1998', 121) AND ShipVia>=2
---
+++ 03_Basic SQL/Scripts/1.1.2_Not_Delivered_Orders.sql
@@ -0,0 +1,8 @@
+﻿SELECT OrderID,
+  CASE
+    WHEN  ShippedDate IS NULL
+    THEN 'Not Shipped'
+    ELSE CAST(ShippedDate AS NVARCHAR(30))
+  END AS ShippedDate
+  FROM [dbo].[Orders]   
+  WHERE ShippedDate IS NULL
