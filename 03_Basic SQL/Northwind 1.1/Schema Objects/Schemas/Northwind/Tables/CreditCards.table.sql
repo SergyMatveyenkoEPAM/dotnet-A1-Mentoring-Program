b3a00e21786689de6235291d15dccf9237e26f08
Sergey Matveyenko <v-smatveyenka@ebsco.com>
2020-03-19 19:26:22 +0300

    task 3.2

---
+++ 03_Basic SQL/Northwind 1.1/Schema Objects/Schemas/Northwind/Tables/CreditCards.table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [Northwind].[CreditCards](
+	[CreditCardId] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	[Number] INT NOT NULL,
+	[ExpiryDate] DATETIME NOT NULL,
+	[CardHolder] NVARCHAR(50) NOT NULL,
+	[EmployeeID] INT NOT NULL	
+)
