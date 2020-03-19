9009d3afcdfefb831b0e9326bb7136115cfd12fa
Sergey Matveyenko <v-smatveyenka@ebsco.com>
2020-03-19 20:29:45 +0300

    task 3.3

--- 03_Basic SQL/Northwind 1.3/Scripts/Post-Deployment/Regions.sql
+++
@@ -1,4 +0,0 @@
-﻿INSERT [Northwind].[Regions] ([RegionID], [RegionDescription]) VALUES (1, N'Eastern                                           ')
-INSERT [Northwind].[Regions] ([RegionID], [RegionDescription]) VALUES (2, N'Western                                           ')
-INSERT [Northwind].[Regions] ([RegionID], [RegionDescription]) VALUES (3, N'Northern                                          ')
-INSERT [Northwind].[Regions] ([RegionID], [RegionDescription]) VALUES (4, N'Southern                                          ')
