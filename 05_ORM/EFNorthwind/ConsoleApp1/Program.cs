3ba60864d627201d90b4cfc7d25abdeb9e1f31e7
Sergey Matveyenko <v-smatveyenka@ebsco.com>
2020-04-01 10:54:45 +0300

    task 5.1

---
+++ 05_ORM/EFNorthwind/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using EFNorthwind.Models;
+using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var context = new NorthwindContext();
+            foreach (var order in context.Orders.Include(o => o.OrderDetails).ThenInclude(od => od.Product).ThenInclude(p => p.Category).Where(o => o.OrderDetails.Any(od => od.Product.Category.CategoryName == "Condiments")))
+            {
+                Console.WriteLine(order.OrderId + " " + order.OrderDate + " " + order.ShipCity + " " + order.ShipCountry + " " + order.OrderDetails?.FirstOrDefault()?.Product.ProductName + " " + order.OrderDetails?.FirstOrDefault()?.Product.Category.CategoryName);
+                Console.WriteLine("OrderDetails");
+                foreach (var orderDetails in order.OrderDetails)
+                {
+                    Console.WriteLine(orderDetails.Product.Category.CategoryName + " " + orderDetails.Product.ProductName + " " + orderDetails.Quantity + " " + orderDetails.UnitPrice);
+                }
+
+                Console.WriteLine("-----------------------------------------------");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
