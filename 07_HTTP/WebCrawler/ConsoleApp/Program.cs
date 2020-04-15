30f80f65d4b3d6a9f9b2be77e82e731755c632b4
Sergey Matveyenko <v-smatveyenka@ebsco.com>
2020-04-15 13:11:40 +0300

    WebCrawler has been created

---
+++ 07_HTTP/WebCrawler/ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Channels;
+using WebCrawler;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var crawler = new Crawler(0,AreaRestriction.CurrentDomainOnly,null,true);
+            crawler.TraceMessage+=(sender,e)=>Console.WriteLine(e.Message);
+
+            crawler.GetSiteData("https://gomel.today/", @"D:\_Here we go");
+
+
+        }
+    }
+}
---
+++ 07_HTTP/WebCrawler/WebCrawler/Crawler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+
+namespace WebCrawler
+{
+    public enum AreaRestriction { Unlimited, CurrentDomainOnly, NotHigherSourcePath }
+
+    public class Crawler
+    {
+        public event EventHandler<OutputMessageEventArgs> TraceMessage;
+        public int MaxLayer { get; set; }
+        public AreaRestriction DomainRestriction { get; set; }
+        public List<string> ResourceTypeRestriction { get; set; }
+        public bool Trace { get; set; }
+
+        public Crawler(int maxLayer, AreaRestriction domainRestriction, List<string> resourceTypeRestriction, bool trace)
+        {
+            MaxLayer = maxLayer;
+            DomainRestriction = domainRestriction;
+            ResourceTypeRestriction = resourceTypeRestriction;
+            Trace = trace;
+        }
+
+
+
+
+
+        public void GetSiteData(string startUrl, string destinationDirectory)
+        {
+            Directory.CreateDirectory(destinationDirectory);
+            string path = Path.Combine(destinationDirectory, (string.IsNullOrEmpty(Path.GetFileName(startUrl)) ? Path.GetFileName(Path.GetDirectoryName(startUrl)) : Path.GetFileName(startUrl)) + ".html");
+            var file = new FileStream(path, FileMode.Create);
+
+
+
+
+
+
+            using var client = new HttpClient();
+
+            client.GetAsync(startUrl).Result.Content.ReadAsStreamAsync().Result.CopyTo(file);
+
+            TraceMessage?.Invoke(this, new OutputMessageEventArgs { Message = "message from lib: everything is fine" });
+        }
+
+    }
+}
