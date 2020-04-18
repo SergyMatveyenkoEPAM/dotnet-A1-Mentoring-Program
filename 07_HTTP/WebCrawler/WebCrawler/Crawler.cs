2699dadfeb2b6dd43e0f85ab99c9ab5b365e33a9
Sergey Matveyenko <v-smatveyenka@ebsco.com>
2020-04-18 13:45:16 +0300

    a few bugs have been fixed

--- 07_HTTP/WebCrawler/WebCrawler/Crawler.cs
+++ 07_HTTP/WebCrawler/WebCrawler/Crawler.cs
@@ -53,51 +53,71 @@ namespace WebCrawler
                 return;
             }
 
-            if (Trace)
-            {
-                TraceMessage?.Invoke(this, new OutputMessageEventArgs { Message = $"found url: {startUri}" });
-            }
-
-            string currentDirectory = Path.Combine(parentDirectory, startUri.Host) + startUri.LocalPath.Replace("/", @"\");
+            string destinationAddress = Path.Combine(parentDirectory, startUri.Host) + startUri.LocalPath.Replace("/", @"\");
             var response = client.GetAsync(startUri).Result;
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return;
+            }
 
             if (response.Content.Headers.ContentType?.MediaType == "text/html")
             {
-                Directory.CreateDirectory(currentDirectory);
-
-                string filePath = Path.Combine(currentDirectory, Path.GetFileName(currentDirectory) + ".html");
-
-                var file = new FileStream(filePath, FileMode.Create);
-
-                var document = new HtmlDocument();
-                document.Load(response.Content.ReadAsStreamAsync().Result, Encoding.UTF8);
-                document.Save(file);
-                file.Close();
-                _retrievedUrls.Add(startUri);
-
-                var internalLinks = document.DocumentNode?.SelectNodes("//@href"/*|//@src"*/)?.Select(n => n.GetAttributeValue("href", n.GetAttributeValue("src", "default")));
-                if (internalLinks.Any())
+                if (Trace)
                 {
-                    foreach (var address in internalLinks)
+                    TraceMessage?.Invoke(this, new OutputMessageEventArgs { Message = $"found url: {startUri}" });
+                }
+
+                SaveHtmlDoc(destinationAddress, response, startUri, client, parentDirectory, layer);
+            }
+            else
+            {
+                if (Trace)
+                {
+                    TraceMessage?.Invoke(this, new OutputMessageEventArgs { Message = $"found file: {startUri}" });
+                }
+
+                SaveFile(destinationAddress, response, startUri);
+            }
+        }
+
+        private void SaveHtmlDoc(string destinationAddress, HttpResponseMessage response, Uri startUri, HttpClient client, string parentDirectory, int layer)
+        {
+            Directory.CreateDirectory(destinationAddress);
+
+            string filePath = Path.Combine(destinationAddress, Path.GetFileName(destinationAddress) + ".html");
+
+            var file = new FileStream(filePath, FileMode.Create);
+
+            var document = new HtmlDocument();
+            document.Load(response.Content.ReadAsStreamAsync().Result, Encoding.UTF8);
+            document.Save(file);
+            file.Close();
+            _retrievedUrls.Add(startUri);
+
+            var internalLinks = document.DocumentNode?.SelectNodes("//@href|//@src")?.Select(n => n.GetAttributeValue("href", n.GetAttributeValue("src", "default")));
+            if (internalLinks != null && internalLinks.Any())
+            {
+                foreach (var address in internalLinks)
+                {
+                    if (Uri.TryCreate(address, UriKind.Absolute, out Uri link))
                     {
-                        if (Uri.TryCreate(address, UriKind.Absolute, out Uri link))
+                        if (link.Scheme == "http" || link.Scheme == "https")
                         {
-                            if (link.Scheme == "http" || link.Scheme == "https")
-                            {
-                                DownloadData(client, link, parentDirectory, layer + 1);
-                            }
+                            DownloadData(client, link, parentDirectory, layer + 1);
                         }
                     }
                 }
             }
-            else
-            {
-                Directory.CreateDirectory(Path.GetDirectoryName(currentDirectory));
-                var file = new FileStream(currentDirectory, FileMode.Create);
-                response.Content.ReadAsStreamAsync().Result.CopyTo(file);
-                file.Close();
-                _retrievedUrls.Add(startUri);
-            }
+        }
+
+        private void SaveFile(string destinationAddress, HttpResponseMessage response, Uri startUri)
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(destinationAddress));
+            var file = new FileStream(destinationAddress, FileMode.Create);
+            response.Content.ReadAsStreamAsync().Result.CopyTo(file);
+            file.Close();
+            _retrievedUrls.Add(startUri);
         }
 
         private bool IsRestricted(Uri startUri)
