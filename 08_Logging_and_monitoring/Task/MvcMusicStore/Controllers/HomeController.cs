cdeadaeb3e4e32d229bb3b18c8bec170509ffb19
Sergey Matveyenko <v-smatveyenka@ebsco.com>
2020-05-03 15:38:41 +0300

    logging

--- 08_Logging_and_monitoring/Task/MvcMusicStore/Controllers/HomeController.cs
+++ 08_Logging_and_monitoring/Task/MvcMusicStore/Controllers/HomeController.cs
@@ -3,16 +3,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using MvcMusicStore.Models;
+using NLog;
 
 namespace MvcMusicStore.Controllers
 {
     public class HomeController : Controller
     {
         private readonly MusicStoreEntities _storeContext = new MusicStoreEntities();
+     
+        private readonly ILogger _logger;
+
+        public HomeController(ILogger logger)
+        {
+            _logger = logger;
+        }
 
         // GET: /Home/
         public async Task<ActionResult> Index()
         {
+            _logger.Debug("Enter to Home page");
+
             return View(await _storeContext.Albums
                 .OrderByDescending(a => a.OrderDetails.Count())
                 .Take(6)
--- 08_Logging_and_monitoring/Task/MvcMusicStore/Global.asax.cs
+++ 08_Logging_and_monitoring/Task/MvcMusicStore/Global.asax.cs
@@ -5,17 +5,45 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Autofac;
+using Autofac.Integration.Mvc;
+using MvcMusicStore.Controllers;
+using NLog;
 
 namespace MvcMusicStore
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private readonly ILogger _logger;
+
+        public MvcApplication()
+        {
+            _logger = LogManager.GetCurrentClassLogger();
+        }
+
+
         protected void Application_Start()
         {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(typeof(HomeController).Assembly);
+            builder.Register(f => LogManager.GetLogger("ForControllers")).As<ILogger>();
+
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(builder.Build()));
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            _logger.Info("Application started");
+        }
+
+        protected void Application_Error()
+        {
+            var ex = Server.GetLastError();
+
+            _logger.Error(ex.ToString());
         }
     }
 }
