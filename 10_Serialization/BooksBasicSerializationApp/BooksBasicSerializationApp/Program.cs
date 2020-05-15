080663708cbfad80e2f84e5a415c4261ef84e21a
Sergey Matveyenko <v-smatveyenka@ebsco.com>
2020-05-15 18:13:18 +0300

    books.xml has been added to the output directory

--- 10_Serialization/BooksBasicSerializationApp/BooksBasicSerializationApp/Program.cs
+++ 10_Serialization/BooksBasicSerializationApp/BooksBasicSerializationApp/Program.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using BooksBasicSerializationLibrary.Models;
+using System;
 using System.IO;
 using System.Xml.Serialization;
-using BooksBasicSerializationLibrary.Models;
 
 namespace BooksBasicSerializationApp
 {
@@ -9,17 +9,15 @@ namespace BooksBasicSerializationApp
     {
         static void Main(string[] args)
         {
-            string path = Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\..\..\" );
-
             XmlSerializer xmlSerializer = new XmlSerializer(typeof(Catalog));
 
-            FileStream stream = new FileStream(path+ "books.xml", FileMode.Open);
+            FileStream stream = new FileStream("books.xml", FileMode.Open);
             Catalog catalog = xmlSerializer.Deserialize(stream) as Catalog;
 
             Console.WriteLine($"Catalog has {catalog.Books.Length} books");
 
             var serializer = new XmlSerializer(typeof(Catalog));
-            var writeStream = new FileStream(path + "books_new.xml", FileMode.Create);
+            var writeStream = new FileStream("books_new.xml", FileMode.Create);
             serializer.Serialize(writeStream, catalog);
             writeStream.Close();
 
