ba147b87da8cbbba37aa596d751cbc5a7305a7ff
Sergey Matveyenko <v-smatveyenka@ebsco.com>
2020-05-15 17:03:19 +0300

    BasicSerialization

---
+++ 10_Serialization/BooksBasicSerializationApp/BooksBasicSerializationLibrary/Models/Catalog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace BooksBasicSerializationLibrary.Models
+{
+    [XmlRoot("catalog", Namespace = "http://library.by/catalog",
+        IsNullable = false)]
+    public class Catalog
+    {
+        [XmlAttribute("date", DataType = "date")]
+        public DateTime Date { get; set; }
+
+        [XmlElement("book", typeof(Book))]
+        public Book[] Books { get; set; }
+    }
+}
---
+++ 10_Serialization/BooksBasicSerializationApp/BooksBasicSerializationLibrary/Models/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Serialization;
+
+namespace BooksBasicSerializationLibrary.Models
+{
+    public enum Genre
+    {
+        Computer,
+        Fantasy,
+        Romance,
+        Horror,
+        [XmlEnum(Name = "Science Fiction")]
+        ScienceFiction,
+    }
+}
